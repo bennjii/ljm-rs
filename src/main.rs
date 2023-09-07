fccd1051fff75e006f1d6d955a0a15e64a9db610
Ben White <contact@bennjii.dev>
2023-09-07 18:47:02 +1000

    mod: time debug util

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 extern crate libloading;
-
+use std::time::Instant;
 use std::{ffi::CString, os::raw::c_double};
 
 use libloading::{Library, Symbol};
@@ -68,10 +68,26 @@ impl LJMWrapper {
 }
 
 fn main() {
+    let now = Instant::now();
+
     let ljm_wrapper = unsafe { LJMWrapper::init() };
 
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
+
+    let now = Instant::now();
+
     let (addr, typ) = ljm_wrapper.name_to_address("AIN0".to_string());
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
+
+    let now = Instant::now();
+
     ljm_wrapper.digital_write_to_address("AIN0".to_string(), "AIN0_RANGE".to_string(), 15_u32);
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 
     println!("Function result: {}:{}", addr, typ);
 }
