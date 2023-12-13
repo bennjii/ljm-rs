9aa6010b39e79030309c5e698fa7926a860a722a
Ben White <contact@bennjii.dev>
2023-12-14 10:47:30 +1100

    mod: fix example structure

--- examples/load.rs
+++ examples/load.rs
@@ -1,24 +1,33 @@
-fn read() {
+extern crate ljmrs;
+
+use std::time::Instant;
+use ljmrs::LJMWrapper;
+
+fn load() {
     let now = Instant::now();
 
-    let ljm_wrapper = unsafe { LJMWrapper::init() };
+    let ljm_wrapper = unsafe { LJMWrapper::init() }.unwrap();
 
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
 
     let now = Instant::now();
 
-    let (addr, typ) = ljm_wrapper.name_to_address("AIN0".to_string());
+    let (addr, typ) = ljm_wrapper.name_to_address("AIN0".to_string()).expect("Expected NTA");
 
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
 
     let now = Instant::now();
 
-    ljm_wrapper.digital_write_to_address("AIN0".to_string(), "AIN0_RANGE".to_string(), 15_u32);
+    ljm_wrapper.write_name(-2, "AIN0_RANGE".to_string(), 15_u32).expect("Expected NTA");
 
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
 
     println!("Function result: {}:{}", addr, typ);
+}
+
+fn main() {
+    load();
 }
--- examples/read.rs
+++ examples/read.rs
@@ -1,26 +1,31 @@
-extern crate ljmrs;
+use std::time::Instant;
+use ljmrs::LJMWrapper;
 
-fn load() {
+fn read() {
     let now = Instant::now();
 
-    let ljm_wrapper = unsafe { LJMWrapper::init() };
+    let ljm_wrapper = unsafe { LJMWrapper::init() }.unwrap();
 
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
 
     let now = Instant::now();
 
-    let (addr, typ) = ljm_wrapper.name_to_address("AIN0".to_string());
+    let (addr, typ) = ljm_wrapper.name_to_address("AIN0".to_string()).expect("Expected AIN0");
 
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
 
     let now = Instant::now();
 
-    ljm_wrapper.digital_write_to_address("AIN0".to_string(), "AIN0_RANGE".to_string(), 15_u32);
+    ljm_wrapper.read_name(-2, "AIN0".to_string()).expect("Expected read to succeed");
 
     let elapsed = now.elapsed();
     println!("Elapsed: {:.2?}", elapsed);
 
     println!("Function result: {}:{}", addr, typ);
+}
+
+fn main() {
+    read();
 }
