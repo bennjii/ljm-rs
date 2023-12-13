e409374baaa5040fbd2fef8a8be747e4738a4aaa
Ben White <contact@bennjii.dev>
2023-12-13 22:42:43 +1100

    mod: get error codes from each function

--- src/ljm/wrapper.rs
+++ src/ljm/wrapper.rs
@@ -92,7 +92,7 @@ impl LJMWrapper {
     /// Returns a tuple of (address, type) in (i32, i32) format.
     /// Verifiable with: -
     /// https://labjack.com/pages/support/?doc=/datasheets/t-series-datasheet/31-modbus-map-t-series-datasheet/
-    pub fn name_to_address(&self, identifier: String) -> (i32, i32) {
+    pub fn name_to_address(&self, identifier: String) -> Result<(i32, i32), LJMError> {
         let n_to_addr: Symbol<extern "C" fn(*const c_char, *mut i32, *mut i32) -> i32> =
             unsafe { self.library.get(b"LJM_NameToAddress").unwrap() };
 
@@ -100,36 +100,38 @@ impl LJMWrapper {
         let mut address: i32 = 0;
         let mut typ: i32 = 0;
 
-        n_to_addr(name.as_ptr(), &mut address, &mut typ);
+        let error_code = n_to_addr(name.as_ptr(), &mut address, &mut typ);
 
-        (address, typ)
+        LJMWrapper::error_code((address, typ), error_code)
     }
 
     /// Digitally writes to address
     /// Takes a handle to the labjack, the name to be written and the value to be written.
     /// Does not return a value.
-    pub fn write_name(&self, handle: i32, name_to_write: String, value_to_write: u32) {
-        let d_write_to_addr: Symbol<extern "C" fn(i32, *const c_char, c_double)> =
+    pub fn write_name(&self, handle: i32, name_to_write: String, value_to_write: u32) -> Result<(), LJMError> {
+        let d_write_to_addr: Symbol<extern "C" fn(i32, *const c_char, c_double) -> i32> =
             unsafe { self.library.get(b"LJM_eWriteName").unwrap() };
 
         let ntw = CString::new(name_to_write).expect("CString conversion failed");
         let vtw = c_double::from(value_to_write);
 
-        d_write_to_addr(handle, ntw.as_ptr(), vtw);
+        let error_code = d_write_to_addr(handle, ntw.as_ptr(), vtw);
+
+        LJMWrapper::error_code((), error_code)
     }
 
     /// Reads from a labjack given the handle and name to read.
     /// Returns an f64 value that is read from the labjack.
-    pub fn read_name(&self, handle: i32, name_to_read: String) -> f64 {
-        let d_read_from_aadr: Symbol<extern "C" fn(i32, *const c_char, c_double)> =
+    pub fn read_name(&self, handle: i32, name_to_read: String) -> Result<f64, LJMError> {
+        let d_read_from_aadr: Symbol<extern "C" fn(i32, *const c_char, c_double) -> i32> =
             unsafe { self.library.get(b"LJM_eReadName").unwrap() };
 
         let ntr = CString::new(name_to_read).expect("CString conversion failed");
         let vtr = c_double::from(0);
 
-        d_read_from_aadr(handle, ntr.as_ptr(), vtr);
+        let error_code = d_read_from_aadr(handle, ntr.as_ptr(), vtr);
 
-        vtr.into()
+        LJMWrapper::error_code(vtr, error_code)
     }
 
     /// Opens a LabJack and returns the handle id as an i32.
