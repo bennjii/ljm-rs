159936259d4203fbbb895208800eac246fa51a9b
Ben White <contact@bennjii.dev>
2024-03-22 15:32:38 +1100

    mod: timers for filling

--- examples/stream.rs
+++ examples/stream.rs
@@ -1,5 +1,7 @@
 extern crate ljmrs;
 
+use std::time::Instant;
+
 use ljmrs::LJMWrapper;
 
 fn stream() {
@@ -9,28 +11,39 @@ fn stream() {
         .open_jack(
             ljmrs::DeviceType::ANY,
             ljmrs::ConnectionType::ANY,
-            "-2".to_string(),
+            "ANY".to_string(),
         )
         .expect("Could not open DEMO LabJack");
 
     println!("Opened LabJack, got handle: {}", open_call);
 
-    let streams = vec!["AIN0", "AIN1"];
+    let streams = vec!["AIN0"];
 
     // Do note that this examples will fail on
     // DEMO mode labjacks, as currently stream mode
     // is not supported for them.
     ljm_wrapper
-        .stream_start(open_call, 1, 1000.0, streams)
+        .stream_start(open_call, 2, 50_000.0, streams)
         .expect("Failed to start stream");
 
     assert!(ljm_wrapper.is_stream_active());
 
-    let read_value = ljm_wrapper
-        .stream_read(open_call)
-        .expect("Could not read values");
+    let now = Instant::now();
 
-    println!("Got: {:?}", read_value);
+    let mut i = 0;
+    while i < 50 {
+        let read_value = ljm_wrapper
+            .stream_read(open_call)
+            .expect("Could not read values");
+
+        println!("Got {}: {:?}", read_value.len(), read_value);
+        
+        // thread::sleep(Duration::from_millis(1));
+        i += 1;
+    }
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
 
     ljm_wrapper.stream_stop(open_call).expect("Expected Value");
 
