617868cfde74c644c63dcd207f4c6596086bde66
Ben White <contact@bennjii.dev>
2024-08-04 14:46:29 +1000

    mod: update lua script as included dependency
    Signed-off-by: Ben White <contact@bennjii.dev>

--- examples/lua.rs
+++ examples/lua.rs
@@ -2,34 +2,7 @@ extern crate ljmrs;
 
 use ljmrs::{LJMLibrary, LuaModule};
 
-const SCRIPT: &str = r#"
-local ramval = 0
-MB.W(46180, 0, ramval)
-local loop0 = 0
-local loop1 = 1
-local loop2 = 2
-
--- Setup an interval to control loop execution speed. Update every second
-LJ.IntervalConfig(0,1000)
-while true do
-  if LJ.CheckInterval(0) then
-    ramval = MB.R(46180, 0)
-
-    if ramval == loop0 then
-      print("using loop0")
-    end
-
-    if ramval == loop1 then
-      print("using loop1")
-    end
-
-    if ramval == loop2 then
-      print("using loop2")
-    end
-
-  end
-end
-"#;
+const SCRIPT: &str = include_str!("example.lua");
 
 fn init() -> i32 {
     unsafe { LJMLibrary::init(None) }.unwrap();
