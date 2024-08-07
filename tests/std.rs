96af4180ec2027921442d2f094c65740ff720c7e
Ben White <contact@bennjii.dev>
2024-08-07 12:51:57 +1000

    mod: update tests to match
    Signed-off-by: Ben White <contact@bennjii.dev>

--- tests/std.rs
+++ tests/std.rs
@@ -1,43 +1,31 @@
 use std::time::Instant;
 
 use ljmrs;
-use ljmrs::{LJMError, LJMErrorCode, LJMLibrary};
-
-fn assert_error(error: LJMError, error_code: i32) {
-    assert!(
-        matches!(
-            error,
-            LJMError::ErrorCode(
-               i, _j
-            ) if i == LJMErrorCode::from(error_code)
-        )
-    );
-}
+use ljmrs::LJMLibrary;
 
 #[test]
 fn open() {
-    let ljm_wrapper = unsafe { LJMLibrary::init(None) }.unwrap();
+    let _ = unsafe { LJMLibrary::init(None) };
 
-    let open_call = ljm_wrapper
-        .open_jack(
-            ljmrs::DeviceType::ANY,
-            ljmrs::ConnectionType::ANY,
-            "-2".to_string(),
-        );
+    let open_call = LJMLibrary::open_jack(
+        ljmrs::DeviceType::ANY,
+        ljmrs::ConnectionType::ANY,
+        "-2".to_string(),
+    );
 
     assert!(open_call.is_ok());
 }
 
 #[test]
 fn get_name() {
-    let ljm_wrapper = unsafe { LJMLibrary::init(None) }.unwrap();
+    let _ = unsafe { LJMLibrary::init(None) };
 
     let mut elapsed_times: Vec<f32> = vec![];
     let addresses = vec!["AIN0", "AIN1", "AIN2", "AIN3", "FIO0", "FIO1"];
 
     for _ in 0..addresses.len() {
         let now = Instant::now();
-        let result = ljm_wrapper.name_to_address("AIN0".to_string());
+        let result = LJMLibrary::name_to_address("AIN0".to_string());
         assert!(result.is_ok());
 
         let elapsed = now.elapsed().as_millis();
@@ -50,20 +38,17 @@ fn get_name() {
 
 #[test]
 fn standard_open_read() {
-    let ljm_wrapper = unsafe { LJMLibrary::init(None) }.unwrap();
+    let _ = unsafe { LJMLibrary::init(None) };
 
-    let open_call = ljm_wrapper
-        .open_jack(
-            ljmrs::DeviceType::ANY,
-            ljmrs::ConnectionType::ANY,
-            "-2".to_string(),
-        )
-        .expect("Could not open DEMO LabJack");
+    let open_call = LJMLibrary::open_jack(
+        ljmrs::DeviceType::ANY,
+        ljmrs::ConnectionType::ANY,
+        "-2".to_string(),
+    ).expect("Could not open DEMO LabJack");
 
     assert_ne!(open_call, -1);
 
-    let read_value = ljm_wrapper
-        .read_name(open_call, "TEST_INT32".to_string())
+    let read_value = LJMLibrary::read_name(open_call, "TEST_INT32".to_string())
         .expect("Expected Value");
 
     assert_eq!(read_value, 0f64);
